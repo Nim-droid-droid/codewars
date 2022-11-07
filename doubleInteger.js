2323ef461cd1ccb87b3e7b4524d737d77917248b
Nim-droid-droid <levkoz2222@outlook.com>
2022-11-07 16:42:22 +0000

    add double integer  challenge solution

---
+++ doubleInteger.js
@@ -0,0 +1,12 @@
+/**/
+
+
+/*
+You Can't Code Under Pressure #1
+Code as fast as you can! You need to double the integer and return it.
+*/
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i*2;
+}
+doubleInteger(10)
