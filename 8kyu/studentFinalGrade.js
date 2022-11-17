28a71c6400106873d744c79cc67e8c770d588d98
Nim-droid-droid <levkoz2222@outlook.com>
2022-11-17 16:37:41 +0000

    add student final grade challenge solution

---
+++ 8kyu/studentFinalGrade.js
@@ -0,0 +1,61 @@
+/*
+Student's Final Grade
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0
+*Use Comparison and Logical Operators.
+https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/javascript 
+*/
+
+
+// function finalGrade (exam, projects) {
+//   //   100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+//     if(exam > 90 || projects > 10){
+//       return 100
+//   // 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+//     }else if(exam > 75 && projects >= 5){
+//       return 90
+//   // 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+//     }else if(exam > 50 && projects >= 2){
+//       return 75
+//     }else{
+//   // 0, in other cases
+//       return 0
+//     }
+// }
+// finalGrade(100, 12)
+
+
+// const finalGrade = (exam, projects) => {
+//   return (
+//     exam > 90 || projects > 10 ? 100 :
+//     exam > 75 && projects >= 5 ? 90 :
+//     exam > 50 && projects >= 2 ? 75 : 0
+//   )
+// }
+// finalGrade(100, 12)
+
+
+const finalGrade = (exam, projects) => exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+finalGrade(100, 12)
+
+
