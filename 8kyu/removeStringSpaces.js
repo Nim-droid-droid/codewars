960d568873bae08b91faa0224e1feedcac0374e9
Nim-droid-droid <levkoz2222@outlook.com>
2022-11-21 17:44:20 +0000

    add no space string challenge solution

---
+++ 8kyu/removeStringSpaces.js
@@ -0,0 +1,8 @@
+/*
+Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+function noSpace(x){
+  return x.split(' ').join('')
+}
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B') //8j8mBliB8gimjB8B8jlB
