faf6a8ccb5253ebaee8e4be027c2708b1707a62a
Nim-droid-droid <levkoz2222@outlook.com>
2022-12-17 22:41:34 +0000

    count positive sum negative challenge solution

---
+++ 8kyu/CountPositivesSumNegatives.js
@@ -0,0 +1,27 @@
+/*
+Count of positives / sum of negatives
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  // increment index for every num > 0
+  // for every num < 0, add them up together
+
+  // !input is not true, meaning input is false = empty/null
+  if (!input || input.length == 0) return [];
+  let sum = 0, count = 0;
+  input.forEach(function(el){ if (el > 0) count++; else sum+=el});
+  return [count, sum];
+}
+countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+
+// REFACTOR
+// use ternary, filter() & reduce()
