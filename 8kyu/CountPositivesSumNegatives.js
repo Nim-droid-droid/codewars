b441c945329dbc590222475788b7932c31ff1c52
Nim-droid-droid <levkoz2222@outlook.com>
2022-12-18 23:23:23 +0000

    update count positive sum negative challenge solution

--- 8kyu/CountPositivesSumNegatives.js
+++ 8kyu/CountPositivesSumNegatives.js
@@ -11,17 +11,23 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 */
 
-function countPositivesSumNegatives(input) {
-  // increment index for every num > 0
-  // for every num < 0, add them up together
+// function countPositivesSumNegatives(input) {
+//   // increment index for every num > 0
+//   // for every num < 0, add them up together
 
-  // !input is not true, meaning input is false = empty/null
-  if (!input || input.length == 0) return [];
-  let sum = 0, count = 0;
-  input.forEach(function(el){ if (el > 0) count++; else sum+=el});
-  return [count, sum];
-}
+//   // !input is not true, meaning input is false = empty/null
+//   if (!input || input.length == 0) return [];
+//   let sum = 0, count = 0;
+//   input.forEach(function(el){ if (el > 0) count++; else sum+=el});
+//   return [count, sum];
+// }
 countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
 
 // REFACTOR
 // use ternary, filter() & reduce()
+// function countPositivesSumNegatives(input) {
+//   return !input || input.length == 0 ? [] : [input.filter(num => num > 0).length, input.filter(num => num < 0).reduce((pre, val) => pre + val, 0)];
+// }
+
+const countPositivesSumNegatives = input => !input || input.length == 0 ? [] : [input.filter(num => num > 0).length, input.filter(num => num < 0).reduce((pre, val) => pre + val, 0)];
+countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
