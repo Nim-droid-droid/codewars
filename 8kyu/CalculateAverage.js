2be5148a92b92fb150b7c33006a1cb75b8488c98
Nim-droid-droid <levkoz2222@outlook.com>
2023-02-20 20:18:12 +0000

    calculate array's average updated challenge solution

--- 8kyu/CalculateAverage.js
+++ 8kyu/CalculateAverage.js
@@ -6,21 +6,23 @@ Write a function which calculates the average of the numbers in a given list.
 Note: Empty arrays should return 0.
 */
 
-  // add up all the nums/el in arr, then divide by arrs length
+// function findAverage(array) {
+//   // add up all the nums/el in arr, then divide by arrs length
   
-//   return array.reduce((acc, el) => Math.ceil((el + el)/array.length), 0);
-function findAverage(array) {
-  // add up all the nums/el in arr, then divide by arrs length
+//   // return array.reduce((acc, el) => Math.ceil((el + el)/array.length), 0);
+//   // add up all the nums/el in arr, then divide by arrs length
   
-  if(array.length === 0){return 0;}
-  // return array.reduce((acc, el) => Math.ceil((el + el)/array.length), 0);
+//   if(array.length === 0){return 0;}
+//   // return array.reduce((acc, el) => Math.ceil((el + el)/array.length), 0);
  
-  // const getSum = array.reduce((sum, el) =>  sum + el,0)
-  // return getSum/array.length;
+//   // const getSum = array.reduce((sum, el) =>  sum + el,0)
+//   // return getSum/array.length;
 
-  const getSum = (array.reduce((sum, el) =>  sum + el, 0) )/array.length
-  return getSum;
-}
+//   const getSum = (array.reduce((sum, el) =>  sum + el, 0) )/array.length
+//   return getSum;
+// }
+
+const findAverage = array => array.length === 0 ? 0 : array.reduce((acc, el)=> acc + el, 0)/array.length ;
 
 findAverage([1,1,1])    //1
 findAverage([1,2,3])    //2
