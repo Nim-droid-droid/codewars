7baaad7f10ff5b128e79a42c5356393ceac13302
Nim-droid-droid <levkoz2222@outlook.com>
2023-02-27 13:07:18 +0000

    used recursion for Factorial challenge

--- 7kyu/Factorial.js
+++ 7kyu/Factorial.js
@@ -7,7 +7,6 @@ Write a function to calculate factorial for a given input. If input is below 0 o
 More details about factorial can be found here.
 */
 
-                    // used recursion for …
 
 function factorial(n){ 
   // with each loop take -1 from n, then * n with fact var
