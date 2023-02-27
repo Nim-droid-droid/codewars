a5fddec6f3ded4231dd7c5ef108a667257ecc0bd
Nim-droid-droid <levkoz2222@outlook.com>
2023-02-27 13:03:20 +0000

    used recursion for Factorial challenge

--- 7kyu/Factorial.js
+++ 7kyu/Factorial.js
@@ -7,31 +7,38 @@ Write a function to calculate factorial for a given input. If input is below 0 o
 More details about factorial can be found here.
 */
 
+                    // used recursion for …
+
 function factorial(n){ 
   // with each loop take -1 from n, then * n with fact var
 
   // cant use reduce, map cuz n is not iterable
 
   // 1 cuz 1 * anything = anything
-  let fact = 1;
-
+  // let fact = 1;
+  
   if(n < 0 || n > 12){
      throw new RangeError();
   }
-  // else isnt necessary, just added it for clarity
-  else{
+// else isnt necessary, just for clarity
+  // else{
     // long as i is less than or equal to 1 for loop will keep iterating & i will increment each loop
     // Y <= & not = cuz i is 1 so not = to n (3)
     // Y <= & not < cuz then it wouldnt include i at 3, it would finish executing at 2nd loop & return 2 instead of 6
-    for(let i=1; i<=n; i++){
-      fact*=i;
-      // fac = 1*1 = 1
-      // fac = 1*2 = 2
-      // fac = 2*3 = 6    the condition is met i (3) is = to n hence for loop finishes executing here
-    }   
-  }
-  return fact;
+    // for(let i=1; i<=n; i++){
+    //   fact*=i;
+    //   // fac = 1*1 = 1
+    //   // fac = 1*2 = 2
+    //   // fac = 2*3 = 6    the condition is met i (3) is = to n hence for loop finishes executing here
+    // }   
+
+    // ? 1  ->  By convention the value of 0! (& 1! factorial) is 1
+      // So if n = 0 or n = 1, return 1
+  return n <= 1 ? 1 : n * factorial(n-1)
+  // }
+//   return fact;
 }
+
 
 factorial(3)    //6    "factorial for 3 is 6"
 factorial(0)    //1    "factorial for 0 is 1"
