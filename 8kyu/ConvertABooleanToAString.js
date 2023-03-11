283587def15ea5fe891f6dcd77ab437612fc4a25
Nim-droid-droid <levkoz2222@outlook.com>
2023-03-11 18:59:54 +0000

    used String() for Convert boolean to str challenge

---
+++ 8kyu/ConvertABooleanToAString.js
@@ -0,0 +1,16 @@
+/*
+Convert a Boolean to a String
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+// function booleanToString(b){
+//   return String(b)
+// }
+
+const booleanToString = b => String(b);
+
+booleanToString(true)    //"true"
+booleanToString(false)    //"false"
