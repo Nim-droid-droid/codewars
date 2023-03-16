f55b8cedf6e4e13bf8df467e1790237eb9aa97e2
Nim-droid-droid <levkoz2222@outlook.com>
2023-03-16 22:41:05 +0000

    used length to return num 5 for 5 without Numbers challenge

---
+++ 8kyu/5WithoutNumbers.js
@@ -0,0 +1,13 @@
+/*
+5 without numbers !!
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+*/
+
+function unusualFive() {
+  return "hello".length;
+}
+unusualFive()    //5
