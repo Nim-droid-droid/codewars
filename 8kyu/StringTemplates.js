28033ec194a64dd44895f4fbc4fe1f6dbbec7c04
Nim-droid-droid <levkoz2222@outlook.com>
2023-03-21 17:35:56 +0000

    used Template literals for String Templates challenge

---
+++ 8kyu/StringTemplates.js
@@ -0,0 +1,15 @@
+/*
+String Templates
+
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+*/
+
+// function buildString(...template){
+//   return `I like ${template.join(', ')}!`;
+// }
+
+const buildString = (...template) => `I like ${template.join(', ')}!`;
+
+buildString('Cheese','Milk','Chocolate')    //'I like Cheese, Milk, Chocolate!', 'Return the correct String'
+buildString('Cheese','Milk')    //'I like Cheese, Milk!', 'Return the correct String'
+buildString('Chocolate')    //'I like Chocolate!', 'Return the correct String'
