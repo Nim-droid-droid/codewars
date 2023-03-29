c1a14c89aaaeb1bb5206c8fb94c72820db792974
Nim-droid-droid <levkoz2222@outlook.com>
2023-03-29 20:14:56 +0100

    used Math.pow() then Exponentiation operator (**) for Squaring an argument challenge

---
+++ 8kyu/SquaringAnArg.js
@@ -0,0 +1,13 @@
+/*
+Function 2 - squaring an argument
+
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// // Write the "square"-function here
+// function square(n){
+// //   return Math.pow(n, 2);
+//   return n **2;
+// }
+
+const square = n => n * n;
