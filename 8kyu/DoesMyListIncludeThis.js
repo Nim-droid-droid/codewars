ecbc0234aae169fd59e339b8f39f7a12c76bdc61
Nim-droid-droid <levkoz2222@outlook.com>
2023-04-15 23:18:48 +0100

    used indexOf() with diff condn for Does My List Include This? Challenge

--- 8kyu/DoesMyListIncludeThis.js
+++ 8kyu/DoesMyListIncludeThis.js
@@ -8,8 +8,11 @@ Create a method that accepts a list and an item, and returns true if the item be
 // }
 // const include = (arr, item) => arr.includes(item)
 
+
 // if index of item (so item is found in arr) so does NOT = -1 (meaning item is present in arr) return true (otherwise return false).
-const include = (arr, item) => arr.indexOf(item) !== -1;
+// const include = (arr, item) => arr.indexOf(item) !== -1;
+
+var include = (a, i) => a.indexOf(i) > -1;
 
 include([1,2,3,4], 3)  // true 
 include([1,2,4,5], 3)  // false
