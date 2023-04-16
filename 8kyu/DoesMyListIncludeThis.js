75518b8fc3bf59e7bc3f507c2377914c3245b9ae
Nim-droid-droid <levkoz2222@outlook.com>
2023-04-16 21:07:14 +0100

    used some() for Does My List Include This? challenge

--- 8kyu/DoesMyListIncludeThis.js
+++ 8kyu/DoesMyListIncludeThis.js
@@ -12,7 +12,12 @@ Create a method that accepts a list and an item, and returns true if the item be
 // if index of item (so item is found in arr) so does NOT = -1 (meaning item is present in arr) return true (otherwise return false).
 // const include = (arr, item) => arr.indexOf(item) !== -1;
 
-var include = (a, i) => a.indexOf(i) > -1;
+// const include = (a, i) => a.indexOf(i) > -1;
+
+// const include = (arr, item) => Boolean(~arr.indexOf(item));
+
+// test to see if some (at least one element) in the arr array is === item
+const include =(arr, item) => arr.some(num => num === item);
 
 include([1,2,3,4], 3)  // true 
 include([1,2,4,5], 3)  // false
