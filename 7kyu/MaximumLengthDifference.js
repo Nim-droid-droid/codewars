3f6919b70adaceabbbcf628804b11f279096eff2
Nim-droid-droid <levkoz2222@outlook.com>
2023-05-05 20:27:36 +0100

    used Math.max Math.min, spread operator, logical operators for Maximum Length Difference challenge

--- 7kyu/MaximumLengthDifference.js
+++ 7kyu/MaximumLengthDifference.js
@@ -59,7 +59,14 @@ const mxdiflg = (a1, a2) => {
   return max;
 */
 
+
+const maxx = (x) => x.map(el => el.length)
+return a1.length === 0 || a2.length === 0 ? -1 : Math.max( Math.max(...maxx(a1)) - Math.min(...maxx(a2)), Math.max(...maxx(a2)) - Math.min(...maxx(a1)) ) 
+
+/* 
 if (a1.length < 1 || a2.length < 1) return -1;
 return Math.max(...a1.reduce((sum, el) => [...sum, ...a2.map((elmnt) => Math.abs(elmnt.length - el.length))], []));
+*/
+
 }
 mxdiflg(["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"])  //13
